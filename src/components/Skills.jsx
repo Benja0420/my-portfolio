8613f74d8f135c33455b84d530f2985732137adf
benja0420 <benjaminsepulveda.0420@gmail.com>
2024-07-23 07:02:16 -0400

    svgs

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,32 +1,52 @@
 import { Element } from "react-scroll";
 import macos from "../assets/icons/macos.svg";
-//import from "../assets/icons/";
 import tailwind from "../assets/icons/tailwind.svg";
 import react from "../assets/icons/react.svg";
+import java from "../assets/icons/java.svg";
+import javascript from "../assets/icons/javascript.svg";
+import node from "../assets/icons/nodejs.svg";
+import spring from "../assets/icons/spring.svg";
+import mysql from "../assets/icons/mysql.svg";
+import git from "../assets/icons/git.svg";
+import linux from "../assets/icons/linux.svg";
+import windows from "../assets/icons/windows.svg";
+import mongo from "../assets/icons/mongodb.svg";
+import html from "../assets/icons/html.svg";
+import css from "../assets/icons/css.svg";
+import express from "../assets/icons/express.svg";
+import docker from "../assets/icons/docker.svg";
+import typescript from "../assets/icons/typescript.svg";
+import next from "../assets/icons/next.svg";
+import kotlin from "../assets/icons/kotlin.svg";
+import swift from "../assets/icons/swift.svg";
 
 const learnedSkills = [
-  { name: "JavaScript", icon: "/assets/icons/javascript.svg" },
+  { name: "HTML", icon: html },
+  { name: "CSS", icon: css },
+  { name: "JavaScript", icon: javascript },
   { name: "React", icon: react },
-  { name: "Node.js", icon: "/assets/icons/nodejs.svg" },
-  { name: "Spring", icon: "/assets/icons/spring.svg" },
-  { name: "Java", icon: "/assets/icons/java.svg" },
-  { name: "MySQL", icon: "/assets/icons/mysql.svg" },
+  { name: "Node.js", icon: node },
+  { name: "Express", icon: express },
+  { name: "Java", icon: java },
+  { name: "Spring", icon: spring },
+  { name: "MySQL", icon: mysql },
+  { name: "MongoDB", icon: mongo },
   { name: "Tailwind CSS", icon: tailwind },
-  { name: "Git", icon: "/assets/icons/git.svg" },
-  { name: "Linux", icon: "/assets/icons/linux.svg" },
+  { name: "Git", icon: git },
+  { name: "Linux", icon: linux },
   { name: "MacOS", icon: macos },
-  { name: "Windows", icon: "/assets/icons/windows.svg" },
+  { name: "Windows", icon: windows },
 ];
 
 const learningSkills = [
-  { name: "Docker", icon: "/assets/icons/docker.svg" },
-  { name: "TypeScript", icon: "/assets/icons/typescript.svg" },
-  { name: "Microservices", icon: "/assets/icons/microservices.svg" },
+  { name: "Next.js", icon: next },
+  { name: "Docker", icon: docker },
+  { name: "TypeScript", icon: typescript },
 ];
 
 const interestedSkills = [
-  { name: "Kotlin", icon: "/assets/icons/kotlin.svg" },
-  { name: "Swift", icon: "/assets/icons/swift.svg" },
+  { name: "Kotlin", icon: kotlin },
+  { name: "Swift", icon: swift },
 ];
 
 const SkillList = ({ title, skills }) => (
@@ -57,7 +77,9 @@ const Skills = () => {
         <div className="grid gap-8 md:grid-cols-3">
           <SkillList title="Learned Skills" skills={learnedSkills} />
           <SkillList title="Learning Skills" skills={learningSkills} />
-          <SkillList title="Skills Interested to Learn" skills={interestedSkills}
+          <SkillList
+            title="Skills Interested to Learn"
+            skills={interestedSkills}
           />
         </div>
       </div>
